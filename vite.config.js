3d3c1a637032f238ca7f8483e8c09ec828f23ebe
simiraaaa <gmcthmy@gmail.com>
2024-02-21 11:36:58 +0900

    feat: config build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default defineConfig(({ command }) => ({
+	plugins: [command === 'build' ? svelte() : sveltekit()],
+
+	// REF: https://stackoverflow.com/questions/75832641/how-to-compile-svelte-3-components-into-iifes-that-can-be-used-in-vanilla-js
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/lib/index.js'),
+			name: 'Components',
+			fileName: 'components',
+		},
+		outDir: 'build',
+	},
+}));
