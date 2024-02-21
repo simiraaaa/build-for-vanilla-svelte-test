e5bc9a7f5cfeda2a6cfe3fd0e84300cde89e7416
simiraaaa <gmcthmy@gmail.com>
2024-02-21 12:06:38 +0900

    test: increment, decrement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <script>
   import Test from '../components/Test.svelte';
+
+  /** @type {Test} */
+  let testComponent;
+
+  function handleIncrement() {
+    console.log('increment');
+  }
+
+  function handleDecrement() {
+    console.log('decrement');
+  }
+
+  function handleClickPlus() {
+    testComponent.incrementCount();
+  }
+
+  function handleClickMinus() {
+    testComponent.decrementCount();
+  }
+
 </script>
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Test name="Test"></Test>
+<Test name="Test" bind:this={testComponent} on:increment={handleIncrement} on:decrement={handleDecrement}></Test>
+<div>
+  <button on:click={handleClickPlus}>+</button>
+  <button on:click={handleClickMinus}>-</button>
+</div>
