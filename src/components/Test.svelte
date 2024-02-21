85bfdacf0a01b7167fd2de37dd8c8c5f8f618ba4
simiraaaa <gmcthmy@gmail.com>
2024-02-21 12:06:11 +0900

    feat: Test increment, decrement

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -1,6 +1,20 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
   /** @type {string} */
   export let name;
+  let count = 0;
+
+  export function incrementCount() {
+    count += 1;
+    dispatch("increment");
+  }
+
+  export function decrementCount() {
+    count -= 1;
+    dispatch("decrement");
+  }
 </script>
 
 <style>
@@ -8,4 +22,5 @@
 
 <div>
   <h1>Hello, {name}!</h1>
+  <p>Count: {count}</p>
 </div>
